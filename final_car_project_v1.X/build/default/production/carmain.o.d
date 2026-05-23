7695b70767a6ebcce84596430ef258c0a35d2062
Mostafa Mohamed Abdelzaher <163563684+MostafaAbd-elzaher@users.noreply.github.com>
2026-05-23 23:35:17 +0300

    abdelzaher_modifications

---
+++ final_car_project_v1.X/build/default/production/carmain.o.d
@@ -0,0 +1,53 @@
+build/default/production/carmain.o.d build/default/production/carmain.o: \
+ carmain.c \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/avr/io.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/avr/sfr_defs.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/inttypes.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/features.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/lib/gcc/avr/5.4.0/include/stdint.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/stdint.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/bits/alltypes.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/bits/stdint.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/mplabx/v6.30/packs/Microchip/ATmega_DFP/3.5.296/xc8/avr/include/avr/iom32.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/avr/portpins.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/avr/common.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/avr/fuse.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/avr/lock.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/util/delay.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/util/delay_basic.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/math.h \
+ /mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/stdio.h
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/avr/io.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/avr/sfr_defs.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/inttypes.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/features.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/stdint.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/bits/alltypes.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/bits/stdint.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/mplabx/v6.30/packs/Microchip/ATmega_DFP/3.5.296/xc8/avr/include/avr/iom32.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/avr/portpins.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/avr/common.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/avr/fuse.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/avr/lock.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/util/delay.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/util/delay_basic.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/math.h:
+
+/mnt/F/important_linux_tools/Microchip_Tools/xc8/v3.10/avr/avr/include/stdio.h:
